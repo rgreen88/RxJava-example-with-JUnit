4dc7990d6bae6ee60dda26f671d568cedf411871
rgreen88 <rynelgreen@gmail.com>
2018-09-17 14:14:21 -0400

    added another layoutconfig for BooksActivity. Multipurpose adapter for different activities,dispose observers at onStop and onDestroy

--- app/src/main/java/com/example/ryne/myrxjava/BooksActivity.java
+++ app/src/main/java/com/example/ryne/myrxjava/BooksActivity.java
@@ -1,4 +1,70 @@
 package com.example.ryne.myrxjava;
 
-public class BooksActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class BooksActivity extends AppCompatActivity{
+
+    private Disposable bookSubscription;
+    private RecyclerView booksRecyclerView;
+    private ProgressBar progressBar;
+    private SimpleStringAdapter stringAdapter;
+    private RestClient restClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        restClient = new RestClient(this);
+        configureLayout();
+        createObservable();
+    }
+
+    //create observables to keep track of list of books
+    private void createObservable(){
+        Observable<List<String>> booksObservable = Observable.fromCallable(() -> restClient
+                .getFavoriteBooks());
+        bookSubscription = booksObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(strings -> displayBooks(strings));
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bookSubscription != null && !bookSubscription.isDisposed()) {
+            bookSubscription.dispose();
+        }
+    }
+    //books using simple adapter class (multi-use adapter!!)
+    private void displayBooks(List<String> books) {
+        stringAdapter.setStrings(books);
+        progressBar.setVisibility(View.GONE);
+        booksRecyclerView.setVisibility(View.VISIBLE);
+    }
+    private void configureLayout() {
+        setContentView(R.layout.activity_books);
+        progressBar = findViewById(R.id.loader);
+        booksRecyclerView = findViewById(R.id.books_list);
+        booksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        stringAdapter = new SimpleStringAdapter(this);
+        booksRecyclerView.setAdapter(stringAdapter);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bookSubscription!=null && !bookSubscription.isDisposed()) {
+            bookSubscription.dispose();
+        }
+    }
 }
