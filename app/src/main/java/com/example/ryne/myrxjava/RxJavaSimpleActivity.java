63483ca6f65c0de4b37e0de5699e636d01170982
rgreen88 <rynelgreen@gmail.com>
2018-09-17 13:19:12 -0400

    created rv adapter SimpleString and implementing rv in rxjavasimple. implementing btn functionality

--- app/src/main/java/com/example/ryne/myrxjava/RxJavaSimpleActivity.java
+++ app/src/main/java/com/example/ryne/myrxjava/RxJavaSimpleActivity.java
@@ -1,4 +1,14 @@
 package com.example.ryne.myrxjava;
 
-public class RxJavaSimpleActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public class RxJavaSimpleActivity extends AppCompatActivity {
+
+    RecyclerView colorViewList;
+    SimpleStringAdapter simpleStringAdapter;
+    CompositeDisposable disposable = new CompositeDisposable(); //disposable to destroy subscriber
+    public int value = 0;
 }
