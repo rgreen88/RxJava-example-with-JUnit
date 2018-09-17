14dae0d11484db1aee36977c530c0fa7c4fdc5ad
rgreen88 <rynelgreen@gmail.com>
2018-09-17 13:35:21 -0400

    added ui update method and button enable/disable during 10 sec sim

--- app/src/main/java/com/example/ryne/myrxjava/RxJavaSimpleActivity.java
+++ app/src/main/java/com/example/ryne/myrxjava/RxJavaSimpleActivity.java
@@ -1,9 +1,18 @@
 package com.example.ryne.myrxjava;
 
+import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaSimpleActivity extends AppCompatActivity {
 
@@ -11,4 +20,35 @@ public class RxJavaSimpleActivity extends AppCompatActivity {
     SimpleStringAdapter simpleStringAdapter;
     CompositeDisposable disposable = new CompositeDisposable(); //disposable to destroy subscriber
     public int value = 0;
+
+    final Observable<Integer> serverDownloadObservable = Observable.create(emitter -> {
+        SystemClock.sleep(10000);//simulate delay
+        emitter.onNext(5);//returns the number 5 after simulated 10 seconds
+        emitter.onComplete();
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjavasimple);
+        View view = findViewById(R.id.button);
+        view.setOnClickListener(v -> {
+            v.setEnabled(false);//disables button until simulation finished
+
+            //Disposable object creation to manage existing subscribers
+            Disposable subscribe = serverDownloadObservable
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(integer -> {
+                       updateTheUserInterface(integer);//method updates ui
+                       v.setEnabled(true);//enables button after simulation
+                    });
+            disposable.add(subscribe);
+        });
+    }
+    //ui update method
+    private void updateTheUserInterface(int integer){
+        TextView view = findViewById(R.id.resultView);
+        view.setText(String.valueOf(integer));
+    }
 }
