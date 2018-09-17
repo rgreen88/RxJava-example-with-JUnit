05dad989f2f588ec0e090bf7d5eb7ea9549f162f
rgreen88 <rynelgreen@gmail.com>
2018-09-17 13:58:30 -0400

    added local color list in color activity, created rv configuration method, marked disposables in onStop

--- app/src/main/java/com/example/ryne/myrxjava/ColorsActivity.java
+++ app/src/main/java/com/example/ryne/myrxjava/ColorsActivity.java
@@ -1,4 +1,60 @@
 package com.example.ryne.myrxjava;
 
-public class ColorsActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    RecyclerView colorListView;
+    SimpleStringAdapter simpleStringAdapter;
+    private Disposable disposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        configureLayout();
+        createObservable();
+    }
+
+    private void createObservable(){
+        Observable<List<String>> listObservable = Observable.just(getColorList());
+        disposable = listObservable.subscribe(colors -> simpleStringAdapter.setStrings(colors));
+    }
+
+    //RV configuration
+    private  void configureLayout(){
+        setContentView(R.layout.activity_colors);
+        colorListView = findViewById(R.id.color_list);
+        colorListView.setLayoutManager(new LinearLayoutManager(this));
+        simpleStringAdapter = new SimpleStringAdapter(this);
+        colorListView.setAdapter(simpleStringAdapter);
+    }
+
+    //ist of colors in rv to view
+    private static List<String> getColorList(){
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("green");
+        colors.add("blue");
+        colors.add("pink");
+        colors.add("brown");
+        return colors;
+    }
+
+    //destroying disposables
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (disposable!=null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
 }
